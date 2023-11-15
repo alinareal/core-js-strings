6600d9da9b0a0f8fc441c72a794dd1cc7c885025
Alina Layevskaya <alina.layevskaya@gmail.com>
2023-11-15 08:25:11 +0300

    feat: implement function isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -343,8 +343,10 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const strProcessed = str.toLowerCase().replace(/[\W_]/g, '');
+  const strReversed = strProcessed.split('').reverse().join('');
+  return strReversed === strProcessed;
 }
 
 /**
